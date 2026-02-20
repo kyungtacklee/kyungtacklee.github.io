4466130edc2ded51afc64781ff715158cca0407e
kyungtacklee <kyungtacklee@gmail.com>
2026-02-20 17:44:54 +0000

    Deploying to gh-pages from @ kyungtacklee/kyungtacklee.github.io@319ce8dd6bde1917fcbd007838af6ada83c54739 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of your cool projects.",
+          description: "projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
